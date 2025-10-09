686275dd2489bf583a61ce77d6b33f9871b10dd2
thomastokmakov24 <thomast89@nycstudents.net>
2025-10-09 12:34:22 -0400

    dsac
    scd

---
+++ app.js
@@ -0,0 +1,22 @@
+const injek = [
+    {
+        cardHeader: "Trail2header",
+        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+        cardPrice: 1,
+        alt:"not good",
+        selected: false,
+        btn:
+    },
+
+]
+function inject(injek) {
+    const container = document.querySelector(".container");
+    container.insertAdjacentHTML("afterbegin",``)
+}
+
+
+function(newStruggle) {
+    for (selected == true in nArray
+}
+const selector = listOfSomeSort.filter((selected )
+if selected = true
