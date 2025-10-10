53580cdc08eadf2f2838ff9ee2b321f160ae80bf
thomastokmakov24 <thomast89@nycstudents.net>
2025-10-10 12:32:21 -0400

    WT
    WER

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-const injek = [
+const Love = [
     {
         cardHeader: "Trail2header",
         cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
         cardPrice: 1,
         alt:"not good",
-        selected: false,
-        btn:
+   //     selected: false,
     },
-
-]
-function inject(injek) {
-    const container = document.querySelector(".container");
-    container.insertAdjacentHTML("afterbegin",``)
+];
+Love.forEach((Love) => inject(Love));
+function inject(cry) {
+    const container =document.querySelector(".container");
+    container.insertAdjacentHTML("afterbegin",``
+        
+    )
 }
 
+// function inject(injek) {
+//     const container = document.querySelector(".container");
+//     container.insertAdjacentHTML("afterbegin",``)
+// }
 
-function(newStruggle) {
-    for (selected == true in nArray
-}
-const selector = listOfSomeSort.filter((selected )
-if selected = true
+
+// function(newStruggle) {
+//     for (selected == true in nArray
+// }
+// const selector = listOfSomeSort.filter((selected )
+// if selected = true
+// function addToCart() {
+//     const buttons = document.querySelectorAll("button");
+//     const btnArray = Array.from(buttons);//create array if need more than forEach.
+//     //console.log(buttons);
+//     btnArray.forEach((btn) => 
+//         btn.addEventListener("click", function(event) {
+//             console.log(event.target.textContent);
+//             console.log(event.target.closest(".display-card").getAttribute("data-title")
+//         );
+//         })
+//     );
+// //closest card to find
+// }//made an array. assign for each to put array of cards on screen. work on add to cart
+// addToCart();
+// //html' in say div class = ''eohugji" 
+// //find item in the array. take that object and push into cart
