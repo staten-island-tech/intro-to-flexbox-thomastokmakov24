cba5f2e057291d7f274fc9bf96a2ec5ede92a7d9
thomastokmakov24 <thomast89@nycstudents.net>
2025-10-22 12:32:44 -0400

    jhhjhh
    hgghggggg

--- app.js
+++ app.js
@@ -1,164 +1,183 @@
 const Love = [
-    {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-   //     selected: false,
-    },
-        {
-        cardHeader: "Trail2header",
-        cardImg: "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
-        cardPrice: 1
-    },
-
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+    //     selected: false,
+  },
+  {
+    cardHeader: "Trail2header",
+    cardImg:
+      "https://th.bing.com/th/id/OIP.WOkEH6d0G3Q4TwlgWplZcwHaGC?w=219&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    cardPrice: 1,
+  },
 ];
 function inject(cry) {
-    const container =document.querySelector(".container");
-    container.insertAdjacentHTML(
-        "afterbegin",
-        `<div class="card">
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
         <img class="cardImg" src=${cry.cardImg} alt="mimimimimi"/>
         <button class = "btn">SGN</button>
         <h2>${cry.cardHeader}</h2> 
         <h2 class = "cardPrice">${cry.cardPrice}</h2>
         </div>`
-        
-    )
+  );
 }
-//The idea is to let _ = 0. Add evnt listner click. If click add the card price of the closest card into _. Add  DIFFERENT BUTTON at the bottom that says check out. Same but if press, instead of adding to _, it would print _. 
+//The idea is to let _ = 0. Add evnt listner click. If click add the card price of the closest card into _. Add  DIFFERENT BUTTON at the bottom that says check out. Same but if press, instead of adding to _, it would print _.
 Love.forEach((Love) => inject(Love));
-const Selectors= {
-    cardHeader: document.querySelector("cardHeader"),
-    cardImg : document.querySelector("cardImg"),
-    cardPrice: document.querySelector("cardPrice")
-}
+const Selectors = {
+  cardHeader: document.querySelector("cardHeader"),
+  cardImg: document.querySelector("cardImg"),
+  cardPrice: document.querySelector("cardPrice"),
+};
 function addToKart() {
-const button = document.querySelectorAll("btn");
-    let stored = 0
-    button.addEventListener("click", function (event) {
-        console.log(/* stored + cardPrice */
-            event.target.closest(".card").getAttribute("cardPrice"),
-            event.textContext
+  const button = document.querySelectorAll(".btn");
+  //  let stored = 0
+  button.addEventListener("click", function (event) {
+    console.log(
+      /* stored + cardPrice */
+      event.target.closest(".card").querySelector(".cardPrice").textContent,
+     // event.target.textContent
+      //Number('cont wahatever' in prev)
+    );
 
-        );
-    })
+  });
 }
 //button.addEventListener("click",addToKart)
-addToKart(Love);
+addToKart();
 
 // function inject(injek) {
 //     const container = document.querySelector(".container");
 //     container.insertAdjacentHTML("afterbegin",``)
 // }
-
 
 // function(newStruggle) {
 //     for (selected == true in nArray
@@ -169,7 +188,7 @@ addToKart(Love);
 //     const buttons = document.querySelectorAll("button");
 //     const btnArray = Array.from(buttons);//create array if need more than forEach.
 //     //console.log(buttons);
-//     btnArray.forEach((btn) => 
+//     btnArray.forEach((btn) =>
 //         btn.addEventListener("click", function(event) {
 //             console.log(event.target.textContent);
 //             console.log(event.target.closest(".display-card").getAttribute("data-title")
@@ -179,5 +198,5 @@ addToKart(Love);
 // //closest card to find
 // }//made an array. assign for each to put array of cards on screen. work on add to cart
 // addToCart();
-// //html' in say div class = ''eohugji" 
+// //html' in say div class = ''eohugji"
 // //find item in the array. take that object and push into cart
