d800153a86a443558b9709c3ab58d16631eab4b6
thomastokmakov24 <thomast89@nycstudents.net>
2025-11-03 12:32:50 -0500

    t
    t

---
+++ Grocer.js
@@ -0,0 +1,18 @@
+const cart = [
+  { name: "Apples", price: 3.5, quantity: 4 },
+  { name: "Milk", price: 4.75, quantity: 2 },
+  { name: "Steak", price: 15.99, quantity: 3 },
+  { name: "Cereal", price: 5.25, quantity: 1 },
+  { name: "Bananas", price: 1.25, quantity: 6 }
+];
+
+function eachDiscount(cart) {
+//  cart.forEach(item => {
+ //   const discountPrice = item.price * 0.9;
+    if (cart.price < 5 ())
+
+}
+function total(anycartorwhatveer){
+return total;
+
+}
