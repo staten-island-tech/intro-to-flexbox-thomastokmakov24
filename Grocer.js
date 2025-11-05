e4640221bc9c3b08c2071c70918bf3bc25b901c2
thomastokmakov24 <thomast89@nycstudents.net>
2025-11-05 12:32:23 -0500

    g
    g

--- Grocer.js
+++ Grocer.js
@@ -5,12 +5,25 @@ const cart = [
   { name: "Cereal", price: 5.25, quantity: 1 },
   { name: "Bananas", price: 1.25, quantity: 6 }
 ];
+function calculateTotalPer(cart) {
+  let total = 0;//great example of function
+  cart.forEach((item) => {
+    let price = item.price
+    if (price < 5) {
+      price = price * 0.95;
+    }
+    total += price * item.quantity;
+  });
+if(total> 100){
+  total = total * 0.9;
+}
+return total;}
 
 function eachDiscount(cart) {
-//  cart.forEach(item => {
+//  cart.forEach(item => {//toFixed(2)
  //   const discountPrice = item.price * 0.9;
-    if (cart.price < 5 ())
-
+    if ( (cart.price < 5)  => (cart.price *0.95) );{
+      else
 }
 function total(anycartorwhatveer){
 return total;
